a0296a420cdb1e054d5ebd46915f3af9e92f988d
Jyoti Rani <jyoti35994@yahoo.com>
2021-09-26 22:12:48 -0700

    React - useStateObject

--- react-advanced-2021/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-2021/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  let details = {
+    name: "Jyoti",
+    age: 27,
+    message: "Random message",
+  };
+  let [oldmsg, newmsg] = useState(details);
+
+  const changetextFunc = () => {
+    newmsg({ ...oldmsg, message: "how are you" });
+  };
+  return (
+    <div>
+      <h4>{oldmsg.name}</h4>
+      <h4>{oldmsg.age}</h4>
+      <h4>{oldmsg.message}</h4>
+      <button className="btn" onClick={changetextFunc}>
+        Change message
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
