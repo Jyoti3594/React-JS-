ac25181e9955f570af4c42e70007b5840d2bfcff
Jyoti Rani <jyoti35994@yahoo.com>
2021-09-28 10:35:17 -0700

    React - useStateCounter

--- react-advanced-2021/src/tutorial/1-useState/setup/5-useState-counter.js
+++ react-advanced-2021/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,57 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+
+  let counterValue = 0 ;
+
+  const [previousval, setNewval] = useState(counterValue);
+
+  const decreaseValue= ()=>{
+     setNewval(previousval-1) ;
+  }
+  const increaseValue= ()=>{
+    setNewval(previousval+1) ;
+  } 
+
+  const resetValue = () =>{
+    setNewval(counterValue)
+    
+  };
+  const complexValue = () => {
+    setTimeout(()=>{
+      setNewval((lastValue)=>{
+          return lastValue +1;
+      });
+        
+    } ,2000);
+  };
+  
+  
+
+  return (
+    <React.Fragment>
+      <section style={{ margin: "4rem 0" }}>
+        <h4>React - useStateCounter</h4>
+        <h1>{previousval}</h1>
+        <button className="btn" onClick={decreaseValue}>
+          Decrese
+        </button>
+        <button className="btn" onClick={increaseValue}>
+          Increase
+        </button>
+        <button className="btn" onClick={resetValue}>
+          Reset
+        </button>
+      </section>
+
+      <section>
+        <h4>More complex counter</h4>
+        <h1>{previousval}</h1>
+        <button className="btn" onClick={complexValue} >Increase later</button>
+      </section>
+    </React.Fragment>
+  );
+
 };
 
 export default UseStateCounter;
