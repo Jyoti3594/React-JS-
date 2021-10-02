398f4637dbc91e2d724992f65c4bde0157030a75
Jyoti Rani <jyoti35994@yahoo.com>
2021-10-01 22:28:49 -0700

    React - useEffect fetch

--- react-advanced-2021/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ react-advanced-2021/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [user, setUser] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const user = await response.json();
+    setUser(user);
+    //console.log(setUser);
+  };
+  
+  useEffect(() => {
+    getUsers();
+    
+  }, [])
+  
+
+  return (
+    <>
+      <h2>GIT users list</h2>;
+      <ul className="users">
+        {user.map((userList) => {
+            const { id, login, avatar_url, html_url } = userList;
+            return <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>;
+        })};
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
