d972d0a65e958c784d12d35250b01042730dbfb5
Jyoti Rani <jyoti35994@yahoo.com>
2021-10-04 16:54:35 -0700

    React - Conditional rendering  - Multiple returns

--- react-advanced-2021/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-2021/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,50 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [Loading, setLoading] = useState(true);
+  const [Error, setError] = useState(false);
+  const [User, setuser] = useState('defaultUser');
+
+  
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => {
+        if(resp.status >=200 && resp.status<=299 ){
+          return resp.json();
+        }
+        else{
+          setLoading(false)
+          setError(true)
+        }
+      })
+
+      .then((User) => {
+        const{login}= User;
+        setuser(login)
+        setLoading(false)
+      })
+      .catch((Error) => console.log(Error));
+  }, [])
+
+  if(Loading){
+    return (
+      <div>
+        <h2>Loading...</h2>;
+      </div>
+  )};
+  if(Error){
+    return (
+      <div>
+        <h2>Error...</h2>;
+      </div>
+  )};
+  return (
+    <div>
+      <h2>{User}</h2>
+    </div>
+  );
+    
+
 };
 
 export default MultipleReturns;
