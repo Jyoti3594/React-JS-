c2c98f64d3e2d2b6fbd3ebfb30d462ecbd7d87e9
Jyoti Rani <jyoti35994@yahoo.com>
2021-10-05 21:35:54 -0700

    React - conditional rendering - short-circuit/Ternary operator

--- react-advanced-2021/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2021/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,26 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, settext] = useState('Hello');
+  const [Error, setError] = useState(false);
+  //const firstValue = text || 'hello world';
+  //const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <React.Fragment>
+    
+      <h1>{text || "Hi "}</h1>
+      {text && <h1>How are you?</h1>}
+      {!text && <h1>Do you like React.js</h1>}
+      <button className="btn" onClick={()=>setError(!Error)}>
+        Toggle Error
+      </button>
+      {Error && <h1>Oops!</h1>}
+      {Error?(
+        <h1>Something Wrong...</h1>
+      ):(<h1>code is working fine...</h1>)}
+    </React.Fragment>
+  );
 };
 
 export default ShortCircuit;
