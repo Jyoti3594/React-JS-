36f7278bea4b950666a6d3479fa7366d2947ea7b
Jyoti Rani <jyoti35994@yahoo.com>
2021-10-06 16:03:43 -0700

    React - conditional rendering -show/hide

--- react-advanced-2021/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ react-advanced-2021/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,44 @@
 import React, { useState, useEffect } from 'react';
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+  return (
+    <React.Fragment>
+      <h2>Show/Hide</h2>
+
+      <button
+        className="btn"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        toggle
+      </button>
+      {show && <Item />}
+    </React.Fragment>
+  );
 };
+  const Item = () => {
+    const [size, setsize] = useState(window.innerWidth);
+
+    const checksize =()=>{
+      setsize(window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize',checksize);
+      return () => {
+        window.removeEventListener('resize', checksize);
+      }
+    }, [])
+      return(
+        <div>
+          <h1>window</h1>
+          <h2>size:{size}px</h2>
+        </div>
+      );
+
+  }
+
 
 export default ShowHide;
