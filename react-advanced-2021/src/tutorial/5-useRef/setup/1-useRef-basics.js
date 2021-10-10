c3d5c1aa4318ed1f95786ac07e7e15e682b1ea92
Jyoti Rani <jyoti35994@yahoo.com>
2021-10-09 18:09:44 -0700

    React - UseRef

--- react-advanced-2021/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ react-advanced-2021/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -4,8 +4,32 @@ import React, { useEffect, useRef } from 'react';
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
-const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+const UseRefBasics = () => { 
+  const refBasic = useRef('Null');
+  const refDiv = useRef('Null');
+  const handleSubmit = (e)=>{
+      e.preventDefault();
+      console.log(refBasic.current.value)
+  }
+  useEffect(() => {
+    console.log(refBasic.current)
+    refBasic.current.focus();
+  }, []);
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <h2>useRef example </h2>
+          <input type="text" ref={refBasic} />
+          <button type="submit" className="btn">
+          Submit
+          </button>
+        </div>
+      </form>  
+      <div ref={refDiv}>Hello World !</div>
+    </>
+  );
 };
 
 export default UseRefBasics;
