36e360b17712e7ad209dbcc10a46bcb9c2370bc4
Jyoti Rani <jyoti35994@yahoo.com>
2021-10-12 16:13:04 -0700

    React -  React.Memo/useMemo/useCallback

--- react-advanced-2021/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ react-advanced-2021/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,21 +1,21 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect,useCallback } from 'react';
 
 export const useFetch = (url) => {
 
     const [loading, setLoading] = useState(true)
     const [products, setProducts] = useState([])
 
-    const getProducts = async () => {
+    const getProducts =usecallback(async () => {
     const response = await fetch(url)
     const products = await response.json()
     setProducts(products)
     setLoading(false)
     console.log(products)
-    }
+    },[url]);
 
     useEffect(() => {
     getProducts()
-    }, [url])
+    }, [url,getProducts])
     return(
         [loading ,products]
     )
