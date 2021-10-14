490faf245495aa5e9f19a64fff7344370f28a284
Jyoti Rani <jyoti35994@yahoo.com>
2021-10-13 22:56:57 -0700

    React - Tour places list project

--- tour-places-list/setup/src/App.js
+++ tour-places-list/setup/src/App.js
@@ -5,7 +5,50 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tour, setTour] = useState([]);
+
+  const removeTour=(id)=>{
+    const newTours=tour.filter((tourlist)=>tourlist.id !== id);
+    setTour(newTours)
+  }
+  const fetchTours = async()=>{
+    setLoading(true)
+    try {
+        const response = await fetch(url)
+        const tour = await response.json()
+        setLoading(false)
+        setTour(tour)
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, [])
+  if(loading){
+    return(
+      <main>
+        <Loading/>
+      </main>
+    )
+  }
+  if(tour.length===0){
+    return(
+      <main>
+        <div className='title'>
+          <h2>No tour left in  the list</h2>
+          <button onClick={fetchTours} className='btn'>Refresh</button>
+        </div>
+      </main>
+    )
+  }
+  return(
+    <main>
+      <Tours tour={tour} removeTour={removeTour}/>
+    </main>
+  )
 }
 
 export default App
